02b7d884720cf42a96c96e53270fa96b770455db
Vaishampayan Reddy <vaisham92@gmail.com>
2017-05-01 21:41:21 -0700

    Adding updated landing page

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ var bodyParser = require('body-parser');
 
 var dataCollector = require('./routes/dataCollector')
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));;
+app.use(express.static(__dirname + '/public'));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(cookieParser())
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -36,16 +39,5 @@ app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
 });
 
